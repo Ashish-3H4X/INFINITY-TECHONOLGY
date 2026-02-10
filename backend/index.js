7b64f34bea1eb1437705d9cdcf2b3ed8f077f20f
Mr Rahul <ashish227087@gmail.com>
2026-02-10 14:30:54 +0530

    backend

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./config/db.js";
+import contactRoute from "./routes/contactRoute.js";
+connectDB();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Infinity Backend is Running 🚀");
+});
+
+app.use("/api/contact", contactRoute);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
